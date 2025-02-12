80a636490eb10cfe157748b33079a97454b3c372
Jiungchu <chujiung00@gmail.com>
2025-02-12 17:42:45 +0900

    [Silver III] Title: N과 M (2), Time: 200 ms, Memory: 18612 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	
+	static void next(int curr, String currString,int n, int m, int count) {
+		if(count==m) {
+			sb.append(currString+"\n");
+			return;
+		}
+		for(int i=curr+1;i<=n;i++) {
+			String nextString = currString+" "+i;
+			next(i, nextString, n, m, count+1);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt(); int m=sc.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			next(i,i+"",n,m,1);
+		}
+		
+		System.out.println(sb.toString());
+		sc.close();
+
+	}
+
+}
