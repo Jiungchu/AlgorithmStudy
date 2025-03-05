f9a251a0d4474a3555dd37755c34a5cb0c438f52
Jiungchu <chujiung00@gmail.com>
2025-03-05 10:16:11 +0900

    [D2] Title: 나무 높이, Time: 93 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,99 +1,65 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
-import java.io.*;
-import java.util.*;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Solution {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	
+	static int N, maxHeight, count1, count2;
+	static int[] trees;
+
+	public static void main(String[] args) throws IOException {
 		int T = Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
-			int n = Integer.parseInt(br.readLine());
-			int[] trees = new int[n];
-			String[] input = br.readLine().split(" ");
-			int maxHeight = 0;
-			for (int i = 0; i < n; i++) {
-				trees[i] = Integer.parseInt(input[i]);
-				if (trees[i] > maxHeight)
-					maxHeight = trees[i];
-			}
-			// 내림차순으로 정렬
-			Arrays.sort(trees);
-			int day = 0;
-			int index = n - 1;
-			int count=0;
-			for (int i = n-1; i >= 0; i--) {
-				if(trees[i]==maxHeight) {
-					index--; count++;
-				}
-				else break;
-			}
-			boolean flag=true;
-			while (count<n) {
-				day++;
-				int grow;
-				if (day % 2 == 1) {
-					grow = 1;
-				} else {
-					grow = 2;
-				}
-				// 뒤에서부터 넣을 수 있는 곳에 넣기
-				for (int i = index; i >= 0; i--) {
-					int tree = trees[i];
-					if(tree==maxHeight-1) flag=true; // 1 더 작은 나무가 있으면 true
-					if(grow==1 && tree==maxHeight-2 && (flag || count==n-1)) { // 1 더 작은 나무가 있을 때
-						continue;
-					}
-					if (tree + grow <= maxHeight) {
-						trees[i] += grow; 
-						if(trees[i]==maxHeight) {
-							count++;
-							if(grow==1) {// 단순 탐색 시작 위치
-								index--; 
-								flag=false;
-							}
-						}
-						break; // 하루에 하나만 grow
-					}
-				}
-//				if(t+1==6) System.out.println("#day: "+day+" "+Arrays.toString(trees)+" "+index+ " "+flag);
-
-			}
-			System.out.println("#" + (t + 1) + " " + day);
+		for (int t = 1; t <= T; t++) {
+			init();
+			count();
+			int ans = solution();
+			sb.append("#").append(t).append(" ").append(ans).append("\n");
 		}
+		System.out.println(sb.toString());
 		br.close();
-
 	}
+	
+	static int solution() {
+		if(count2==0) return getDate(); // count2==0이면 바로 값 출력
+		int prevDate = Integer.MAX_VALUE;
+		int currDate;
+		// count를 변화했을 때 값이 더 커지는 경우
+		while((currDate = getDate())<prevDate) {
+			count1 += 2;
+			count2 --;
+			prevDate = currDate;
+		}
+		return prevDate;
+	}
+	
+	static int getDate() {
+		if(count1>count2) return count1*2-1;
+		else return count2*2; // count2가 크거나 같으면 count2를 기준으로 
+	}
+	
+	static void count() {
+		for(int i=0;i<N;i++) {
+			int diff = maxHeight-trees[i];
+			count1 += diff%2;
+			count2 += diff/2;
+		}
+	}
+
+	static void init() throws IOException{
+		N = Integer.parseInt(br.readLine());
+		trees = new int[N];
+		count1=count2=0;
+		maxHeight=0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			int num = Integer.parseInt(st.nextToken());
+			trees[i]=num;
+			maxHeight = Math.max(maxHeight, num);
+		}
+	}
+
 }
