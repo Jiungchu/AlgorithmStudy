f1ea48a38186eeb4a09a3659c2375ea42d544e97
Jiungchu <chujiung00@gmail.com>
2025-03-06 14:27:33 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 251 ms, Memory: 28,956 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -4,111 +4,89 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	
-	static int d, w, k, t;
-	static int[][] grid;
-	static int[] row, col;
-	static boolean success;
+    static int d, w, k;
+    static int[][] grid;
+    static int[] row;
+    static boolean success;
+    static int min;
+    static int t;
 
-	public static void main(String[] args) throws Exception {
-		int T = Integer.parseInt(br.readLine());
-		for (t = 1; t <= T; t++) {
-			init();
-			solution();
-		}
-		System.out.println(sb.toString());
-		br.close();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            d = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+            grid = new int[d][w];
+            row = new int[d];
+            Arrays.fill(row, -1);
+            success = false;
+            min = k + 1;
+            for (int i = 0; i < d; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < w; j++) {
+                    grid[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            if (k == 1) {
+                System.out.println("#" + (t + 1) + " " + 0);
+                continue;
+            }
+            dfs(0, 0);
+            System.out.println("#" + (t + 1) + " " + min);
+        }
+        br.close();
+    }
 
-	}
-	
-	static void solution() {
-		if(k==1) { // k==1이면 바로 출력
-			sb.append("#").append(t).append(" ").append(0).append("\n");
-			return;
-		}
-		for (int i = 0; i <= k; i++) {
-			dfs(0, 0, i);
-			if (success) {
-				sb.append("#").append(t).append(" ").append(i).append("\n");
-				return;
-			}
-		}
-	}
+    static void dfs(int curr, int count) {
+        if (count >= min || count > k)
+            return;
+        if (count == k) {
+            min = Math.min(min, count);
+            return;
+        }
+        if (curr == d) {
+            if (check()) min = count;
+            return;
+        }
 
-	static void dfs(int curr, int count, int maxCount) {
-		// 남은 행을 다 바꿔도 maxCount가 되지 앟는 경우
-		if (success || count + (d - curr) < maxCount)
-			return;
-		if (count == maxCount) {
-			if (check())
-				success = true;
-		} else {
-			for (int i = curr; i < d; i++) {
-				if (row[i] != -1)
-					continue;
-				for (int type : new int[] { 0, 1 }) {
-					row[i] = type;
-					dfs(i + 1, count + 1, maxCount); // 현재 바꾼 행의 다음 행부터 바꾸기
-				}
-				row[i] = -1;
-			}
-		}
-	}
+        dfs(curr + 1, count);
+        row[curr] = 0;
+        dfs(curr + 1, count + 1);
 
-	static boolean check() {
-		int pass = 0;
-		int[] currCol = Arrays.copyOf(col,col.length);
-//		System.out.println(Arrays.toString(currCol));
-		for(int r=0;r<d;r++) {
-			if(row[r]==-1) continue;
-			for(int c=0;c<w;c++) {
-				if(row[r]==1) currCol[c] = currCol[c] | (1<<r); 
-				else currCol[c] &= ~(1<<r); 
-			}
-		}
-//		System.out.println(Arrays.toString(currCol));
-		for(int c=0;c<w;c++) {
-			int col = currCol[c];
-			int checkVal = (1<<k)-1;
-			while(checkVal < (1<<d)-1) {
-				if(((col&checkVal)==0) || (col&checkVal)==checkVal) {
-					pass++;
-					break;
-				}
-				checkVal <<=1;
-			}
-			if(pass != c+1) return false;
-		}
-		return true; 
-	}
-	
-	static void init() throws IOException{
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		d = Integer.parseInt(st.nextToken());
-		w = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		grid = new int[d][w];
-		row = new int[d];
-		col = new int[w];
-		Arrays.fill(row, -1);
-		success = false;
-		for (int i = 0; i < d; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < w; j++) {
-				grid[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		// 열 정보를 정수 하나로 관리
-		for(int j=0;j<w;j++) {
-			int hashVal = 0;
-			// 뒤에서부터 저장. i번 열의 정보는 i번 비트에 저장됨
-			for(int i=d-1;i>=0;i--) {
-				hashVal <<=1;
-				hashVal+=grid[i][j];
-			}
-			col[j] = hashVal;
-		}
-	}
+        row[curr] = 1;
+        dfs(curr + 1, count + 1);
+
+        row[curr] = -1;
+    }
+
+    static boolean check() {
+        int pass = 0;
+        for (int j = 0; j < w; j++) {
+            int count = 1;
+            int prev = row[0] == -1 ? grid[0][j] : row[0];
+            for (int i = 1; i < d; i++) {
+                int curr = (row[i] == -1) ? grid[i][j] : row[i];
+
+                if (prev == curr) {
+                    count++;
+                } else {
+                    count = 1;
+                }
+                prev = curr;
+
+                if (k >= d - i + 1 && count == 1)
+                    break;
+                if (count == k) {
+                    pass++;
+                    break;
+                }
+            }
+            if (pass != j + 1)
+                return false;
+        }
+        return true;
+    }
 }
