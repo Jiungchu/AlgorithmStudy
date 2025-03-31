0727691a231006a3e8237c45825ed7717acf06e9
Jiungchu <chujiung00@gmail.com>
2025-03-31 15:14:00 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 194 ms, Memory: 30,476 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -1,97 +1,107 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Solution {
-	static int[][] grid;
-	static int[] dr = { 1, -1, 0, 0 };
-	static int[] dc = { 0, 0, 1, -1 };
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+
+	static int N, t, minSum, maxConnected;
+	static int[][] map;
 	static List<int[]> cores;
-	static int n, t, maxCount, minLength;
+	// 오른쪽부터 시계방향으로 돌기
+	static int[] dr = { 0, 1, 0, -1 }, dc = { 1, 0, -1, 0 };
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		for (t = 0; t < T; t++) {
-			// 값 입력, cores에 외곽에 위치하지 않은 코어들 저장
-			n = Integer.parseInt(br.readLine());
-			grid = new int[n][n];
-			cores = new ArrayList<>();
-			maxCount = 0; minLength = 99999;
-			for (int i = 0; i < n; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < n; j++) {
-					int num = Integer.parseInt(st.nextToken());
-					if (num == 1 && i != 0 && j != 0) {
-						cores.add(new int[] { i, j });
-					}
-					grid[i][j] = num;
-				}
-			}
-			dfs(0, 0, 0);
-			System.out.println("#" + (t + 1) + " " + minLength);
+		for (t = 1; t <= T; t++) {
+			init();
+			solution(0, 0, 0);
+			sb.append("#").append(t).append(" ").append(minSum).append("\n");
 		}
+		System.out.println(sb.toString());
 		br.close();
 	}
 
-	static void dfs(int index, int count, int length) {
-		// index = 코어 번호, count = 현재 담긴 코어의 수, length = 현재 전선 길이의 합
-		if (index == cores.size()) {
-			if (count > maxCount) {
-				maxCount = count;
-				minLength = length;
-			} else if (count == maxCount)
-				minLength = Math.min(minLength, length);
-			return;
+	static void solution(int index, int connected, int length) {
+		if(connected==maxConnected && (minSum>length)) {
+				minSum= length;  // 최대로 연결된 경우 짧은 쪽으로 update
 		}
-		// 가장 많은 코어를 담는 경우는 길이를 저장, 코어의 수가 동일하면 최소값을 저장
-		int r = cores.get(index)[0];
-		int c = cores.get(index)[1];
-		for (int d = 0; d < 4; d++) {
-			int currLength = lengthCalculate(r, c, d);
-			if (currLength != -1) {
-				// 경로가 있는 경우 포함하고 재귀호출
-				dfs(index + 1, count + 1, length + currLength);
-				removeTrace(r + dr[d] * currLength, c + dc[d] * currLength, d); // 다음 재귀호출을 위해 경로 지우기
+		else if(connected>maxConnected) { // 연결된 코어의 수가 늘어나면 무조건 update
+			maxConnected = connected;
+			minSum = length;
+		}
+		// 모든 코어를 다 고려한 경우 return
+		if(index>=cores.size()) return;
+		
+		int r = cores.get(index)[0], c = cores.get(index)[1];
+		for(int d=0;d<4;d++) {
+			int curLen = drawLine(r,c,d);
+			if(curLen>0) {
+				solution(index+1, connected+1, length+curLen);
+				removeLine(r,c,d); // 백트래킹을 위해 경로 지우기
 			}
 		}
-		// 해당 core를 연결하지 않는 경우
-		dfs(index + 1, count, length);
+		// 연결하지 않은 경우의 수로 넘기기
+		solution(index+1, connected, length);
 	}
 
-	static int lengthCalculate(int r, int c, int d) {
+	// 음수로 line을 표현
+	static int drawLine(int r, int c, int d) {
 		int length = 0;
-		// 한 방향으로 쭉 직진
-		int nr=r; int nc=c;
-		while (true) {
-			nr = nr + dr[d];
-			nc = nc + dc[d];
-			if (nr >= 0 && nr < n && nc >= 0 && nc < n) {
-//				System.out.println("#"+r+","+c+" "+nr+" "+nc+ " "+grid[nr][nc]);
-				if (grid[nr][nc] == 0) {
-					length++;
-					grid[nr][nc] = 2; // 전선은 2로 표시
-				} else {
-					// 길이 막힌 경우, 지금까지 지나온 경로 지우고 -1 return
-					removeTrace(nr - dr[d], nc - dc[d], d);
-					return -1;
+		int nr = r + dr[d], nc = c + dc[d];
+		while (nr >= 0 && nr < N && nc >= 0 && nc < N) {
+			if (map[nr][nc] != 0) {
+				removeLine(nr, nc, (d + 2) % 4); // 거꾸로 돌아가서 라인 지우기
+				return -1;
+			}
+			map[nr][nc]--;
+			nr += dr[d];
+			nc += dc[d];
+			length++;
+		}
+		return length;
+	}
+	
+	// 라인 지우기
+	static void removeLine(int r, int c, int d) {
+		int nr = r + dr[d], nc = c + dc[d];
+		while (nr >= 0 && nr < N && nc >= 0 && nc < N && map[nr][nc] != 1) {
+			map[nr][nc]++;
+			nr += dr[d];
+			nc += dc[d];
+		}
+	}
+
+	static void arrayShow(int[][] arr) {
+		System.out.println("#######");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(Arrays.toString(arr[i]));
+		}
+	}
+
+	static void init() throws IOException {
+		N = Integer.parseInt(br.readLine());
+		minSum = Integer.MAX_VALUE;
+		maxConnected = 0;
+		map = new int[N][N];
+		cores = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				int num = Integer.parseInt(st.nextToken());
+				map[i][j] = num;
+				if (num == 1 && i != 0 && j != 0 && i != N - 1 && j != N - 1) { // 가장자리 core는 아예 고려 x
+					cores.add(new int[] { i, j });
 				}
-			} else {
-				// 맵의 끝에 도달했으므로 연결 가능
-				return length;
 			}
 		}
+
 	}
 
-	// [r,c]에서 d방향의 반대로 core를 만날 때까지 경로를 지우는 함수
-	static void removeTrace(int r, int c, int d) {
-		while (true) {
-			if (grid[r][c] == 1)
-				return;
-
-			grid[r][c] = 0;
-			r -= dr[d];
-			c -= dc[d];
-		}
-	}
 }
