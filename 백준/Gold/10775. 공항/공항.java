84d599fd286c4dcc3c94083eded7498eba1cfe1e
Jiungchu <chujiung00@gmail.com>
2025-04-01 10:41:32 +0900

    [Gold II] Title: 공항, Time: 180 ms, Memory: 21460 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.java
+++ 백준/Gold/10775. 공항/공항.java
@@ -2,13 +2,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.TreeSet;
 
 public class Main {
 	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static int G, P;
-	static TreeSet<Integer> set;
+	static int G, P, parents[];
 	
 	public static void main(String[] args) throws IOException{
 		init();
@@ -18,9 +18,10 @@ public class Main {
 	static void solution() throws IOException {
 		int count=0;
 		for(int i=0;i<P;i++) {
-			Integer g = set.floor(Integer.parseInt(br.readLine()));
-			if(g==null) break;
-			set.remove(g);
+			int num = find(Integer.parseInt(br.readLine()));
+			if(num==0) break;
+			// 현재 점 사용 처리. 아래 점과 연결
+			union(num, num-1);
 			count++;
 		}
 		System.out.println(count);
@@ -29,10 +30,20 @@ public class Main {
 	static void init() throws IOException {
 		G = Integer.parseInt(br.readLine());
 		P = Integer.parseInt(br.readLine());
-		set = new TreeSet<>();
+		parents = new int[G+1];
 		for(int i=1;i<=G;i++) {
-			set.add(i);
+			parents[i] = i;
 		}
+	}
+	
+	// 유니온 파인드를 통해 g_i가 들어왔을 때, 대응되는 가장 큰 점을 바로 return
+	static int find(int x) {
+		if(x==parents[x]) return x;
+		return parents[x] = find(parents[x]);
+	}
+	
+	static void union(int a, int b) {
+		parents[find(a)] = parents[find(b)];
 	}
 
 }
