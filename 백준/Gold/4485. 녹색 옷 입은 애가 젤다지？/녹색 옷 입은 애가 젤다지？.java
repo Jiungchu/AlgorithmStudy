0335a6c85a96dccd6b5ee45dd6d47d7b016f20d9
Jiungchu <chujiung00@gmail.com>
2025-04-01 13:16:56 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 292 ms, Memory: 23292 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,66 +1,61 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-public class Main{
 
-	static String[][] costs;
-	static int[][] path;
-	static int[] dr = { 1, -1, 0, 0 };
-	static int[] dc = { 0, 0, 1, -1 };
+public class Main {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n;
-		int t = 0;
-		while ((n = Integer.parseInt(br.readLine())) != 0) {
-			t++;
-			costs = new String[n][n];
-			path = new int[n][n];
-			for (int i = 0; i < n; i++) {
-				costs[i] = br.readLine().split(" ");
-				Arrays.fill(path[i], -1);
-			}
-
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n ; j++) {
-					// 값 넣기
-					// 기본적으로 위, 왼쪽 값 중 작은 값을 기반으로 넣기
-					if (i == 0 && j == 0)
-						path[i][j] = Integer.parseInt(costs[i][j]);
-					else if (i == 0)
-						path[i][j] = Integer.parseInt(costs[i][j]) + path[i][j - 1];
-					else if (j == 0)
-						path[i][j] = Integer.parseInt(costs[i][j]) + path[i - 1][j];
-					else {
-						int path1 = path[i - 1][j];
-						int path2 = path[i][j - 1];
-						path[i][j] = Math.min(path1, path2) + Integer.parseInt(costs[i][j]);
-					}
-					update(i, j);
-				}
-			}
-			System.out.println("Problem " + t + ": " + path[n - 1][n - 1]);
-		}
-		br.close();
-
-	}
-
-	// 깊이 우선으로 업데이트
-	static void update(int r, int c) {
-		int n = path.length;
-		for (int i = 0; i < 4; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-			// 갱신이므로 아직 방문하지 않은 점의 경우 업데이트 x
-			if (nc >= 0 && nc < n && nr >= 0 && nr < n && path[nr][nc] != -1) {
-				int newCost = path[r][c] + Integer.parseInt(costs[nr][nc]);
-				if (path[nr][nc] > newCost) {
-					path[nr][nc] = newCost;
-					update(nr, nc);
-				}
-			}
-		}
-	}
-
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	
+	static int N, map[][], costs[][], t;
+	
+    public static void main(String[] args) throws IOException{
+    	N = Integer.parseInt(br.readLine()); 
+    	t=0;
+    	while(N!=0) {
+    		init();
+    		solution();
+    		N = Integer.parseInt(br.readLine());
+    	}
+    	System.out.println(sb.toString());
+    }
+    
+    static int[] dr= {1,-1,0,0}, dc= {0,0,1,-1};
+    
+    static void solution() {
+    	PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[2]-b[2]));
+    	pq.add(new int[] {0,0,map[0][0]});
+    	while(!pq.isEmpty()) {
+    		int[] curr = pq.poll();
+    		int r = curr[0], c = curr[1], cost = curr[2];
+    		if(costs[r][c]<cost) continue;
+    		costs[r][c] = cost;
+//    		System.out.println(r+" "+c+" "+cost);
+    		for(int d=0;d<4;d++) {
+    			int nr = r+dr[d]; int nc=c+dc[d];
+    			if(nr<0 || nr>=N || nc<0 || nc>=N) continue;
+    			if(costs[nr][nc]>cost+map[nr][nc]) {
+    				costs[nr][nc]=cost+map[nr][nc];
+    				pq.add(new int[] {nr,nc,costs[nr][nc]});
+    			}
+    		}
+    	}
+    	sb.append("Problem ").append(++t).append(": ").append(costs[N-1][N-1]).append("\n");
+    }
+    
+    static void init() throws IOException {
+    	map = new int[N][N];
+    	costs = new int[N][N];
+    	for(int i=0;i<N;i++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine());
+    		for(int j=0;j<N;j++) {
+    			map[i][j] = Integer.parseInt(st.nextToken());
+    			costs[i][j] = Integer.MAX_VALUE;
+    		}
+    	}
+    }
 }
