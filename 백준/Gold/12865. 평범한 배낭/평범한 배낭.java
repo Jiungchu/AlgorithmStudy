675fb947c4842f0d53a21dddf60b2516671f2d3f
Jiungchu <chujiung00@gmail.com>
2025-04-07 11:03:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 108 ms, Memory: 12076 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,32 +1,43 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-		
-		
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	static int N, K, items[][], dp[];
+	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] nk = br.readLine().split(" ");
-		int n = Integer.parseInt(nk[0]);
-		int k = Integer.parseInt(nk[1]);
-		
-		List<int[]> list = new ArrayList<>();
-		for(int i =0;i<n;i++) {
-			String[] input = br.readLine().split(" ");
-			list.add(new int[] {Integer.parseInt(input[0]),Integer.parseInt(input[1])});
-		}
-		
-		int[] dp = new int[k+1];
-		for(int[] item:list) {
-			int weight = item[0]; int value = item[1];
-			for(int w=k;w>=weight;w--) {
-				dp[w] = Math.max(dp[w], dp[w-weight]+value);
+		init();
+		solution();
+	}
+	
+	static void solution() {
+		dp = new int[K+1];
+		for(int i=0;i<N;i++) {
+			int[] item = items[i];
+			int w = item[0], v=item[1];
+			for(int j=K; j>=w;j--) {
+				dp[j] = Math.max(dp[j], dp[j-w]+v);
 			}
 		}
-		System.out.println(dp[k]);
-		br.close();
+		System.out.println(dp[K]);
+	}
+	
+	static void init() throws IOException{
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		items = new int[N][2];
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			items[i][0] = Integer.parseInt(st.nextToken());
+			items[i][1] = Integer.parseInt(st.nextToken());
+		}
 	}
 
 }
- 
