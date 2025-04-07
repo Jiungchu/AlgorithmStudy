49179f39305baf047e69b0dcfe3c6f079c8e0d83
Jiungchu <chujiung00@gmail.com>
2025-04-07 11:44:24 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 68 ms, Memory: 11784 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,81 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static class Pipe {
+		int lr, lc, rr, rc, d;
+
+		public Pipe(int lr, int lc, int rr, int rc, int d) {
+			super();
+			this.lr = lr;
+			this.lc = lc;
+			this.rr = rr;
+			this.rc = rc;
+			this.d = d;
+		}
+
+		@Override
+		public String toString() {
+			return "Pipe [lr=" + lr + ", lc=" + lc + ", rr=" + rr + ", rc=" + rc + ", d=" + d + "]";
+		}
+		
+	}
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	static int N, map[][], dp[][][];
+	
+	public static void main(String[] args) throws IOException{
+		init();
+		solution();
+	}
+	
+	static int[][][] dir = {
+			{{ 0,1,0,1,0},{0,1,1,1,2}}, // 가로 방향
+			{{ 1,0,1,0,1},{1,0,1,1,2}}, // 세로 방향
+			{{ 1,1,0,1,0},{1,1,1,0,1},{1,1,1,1,2}} // 대각선
+	};
+	
+	static void solution() {
+		// 행, 열, 방향별로 dp 시용 
+		int[][][] dp = new int[N][N][3];
+		dp[0][1][0] = 1;
+
+		for (int r=0; r<N;r++) {
+		    for (int c=2; c<N;c++) {
+		        if (map[r][c] == 1) continue;
+
+		        // 이전 시점에 가로, 대각선이었을 경우 가로 가능 
+		        dp[r][c][0] = dp[r][c-1][0]+dp[r][c-1][2];
+
+		        // 이전 시점에 세로, 대각선이었을 때 세로 가능
+		        if (r>0)
+		            dp[r][c][1] = dp[r-1][c][1]+dp[r-1][c][2];
+
+		        // 대각선은 모든 경우에서 가능. 단 벽을 긁으면 안됨
+		        if (r > 0 && map[r-1][c] == 0 && map[r][c-1] == 0)
+		            dp[r][c][2] = dp[r-1][c-1][0] + dp[r-1][c-1][1] + dp[r-1][c-1][2];
+		    }
+		}
+
+		int result = dp[N-1][N-1][0] + dp[N-1][N-1][1] + dp[N-1][N-1][2];
+		System.out.println(result);
+	}
+	
+	static void init() throws IOException {
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		for(int i=0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+	}
+}
