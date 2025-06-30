a93657915a28b67aaa7f239303ff89591816ba9b
Jiungchu <chujiung00@gmail.com>
2025-06-30 21:19:02 +0900

    [Gold V] Title: 회의실 배정, Time: 424 ms, Memory: 65360 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,58 +1,76 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-
 public class Main {
+
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	// 회의가 끝나는 시간을 key로 하고, 회의 시작 시간이 담긴 우선순위 큐를 사용
-	static Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
-	static int N, ans;
+
+	// 회의가 끝나는 시간을 기준으로 묶기
+	static int N;
+	static Map<Integer, Integer> zeroCount, shortCons;
+	static List<Integer> endtimes;
 
 	public static void main(String[] args) throws IOException {
 		init();
 		solution();
-		System.out.println(ans);
-		br.close();
-	}
-
-	static void solution() {
-		List<Integer> sortedKeys = new ArrayList<>(map.keySet());
-		Collections.sort(sortedKeys);
-		int prevEnd = 0;
-		for (int endTime : sortedKeys) {
-			// 현재 종료 시간 중 가장 늦게 시작하는 회의
-			int startTime = map.get(endTime).poll();
-			// 시작과 끝이 같은 회의가 있는 경우
-			while (startTime == endTime && !map.get(endTime).isEmpty()) {
-				startTime = map.get(endTime).poll();
-				ans++;
-			}
-			if (startTime < prevEnd)
-				continue; // 마지막 회의가 끝난 시간보다 늦게 시작한 경우
-			ans++;
-			prevEnd = endTime;
-		}
 	}
 
 	static void init() throws IOException {
 		N = Integer.parseInt(br.readLine());
+		zeroCount = new HashMap<>();
+		shortCons = new HashMap<>();
+		endtimes = new ArrayList<>();
+
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
-			// 내림차순으로 정렬되도록 우선순위 큐 사용
-			if (!map.containsKey(end))
-				map.put(end, new PriorityQueue<>((a, b) -> -Integer.compare(a, b)));
-			map.get(end).add(start);
+
+			// 끝나는 시간을 기준으로 그룹핑
+			if (!shortCons.containsKey(end)) {
+				zeroCount.put(end, 0);
+				shortCons.put(end, Integer.MIN_VALUE);
+				endtimes.add(end);
+			}
+			// 끝나는 시간이 같은 회의 중 가장 짧은 회의만 저장
+			// 시작 시간과 끝나는 시간이 같지 않은 이상 끝나는 시간이 같은 회의는 하나만 있음
+			if (start == end)
+				zeroCount.put(end, zeroCount.get(end) + 1);
+			else if (shortCons.get(end) < start) {
+				shortCons.put(end, start);
+			}
 		}
 	}
 
+	static void solution() {
+		int count = 0;
+		int lastTime = 0;
+
+		// 끝나는 시간이 빠른 회의부터 순서대로 확인
+		Collections.sort(endtimes);
+		for (int time : endtimes) {
+
+			int startTime = shortCons.get(time);
+			if (startTime != Integer.MIN_VALUE && startTime >= lastTime) {
+				count++;
+				lastTime = time;
+			}
+
+			// 시작 시간과 끝나는 시간이 같은 회의는 바로 count
+			int zero = zeroCount.get(time);
+			count += zero;
+			if(zero!=0) lastTime = time;
+
+		}
+		System.out.println(count);
+	}
 }
